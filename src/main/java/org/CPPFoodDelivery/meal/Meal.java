3dd411527487d5fdc148718fdf4112cabd0e070b
Some-Regular-Person <31082683+Justin-J-K@users.noreply.github.com>
2024-05-15 16:16:19 -0700

    Implement meal and factory

--- src/main/java/org/CPPFoodDelivery/meal/Meal.java
+++ src/main/java/org/CPPFoodDelivery/meal/Meal.java
@@ -1,4 +1,50 @@
 package org.CPPFoodDelivery.meal;
 
-public class Meal {
+import org.CPPFoodDelivery.meal.macros.carb.Carb;
+import org.CPPFoodDelivery.meal.macros.fat.Fat;
+import org.CPPFoodDelivery.meal.macros.protein.Protein;
+
+public class Meal implements Cloneable {
+    private Carb carb;
+    private Fat fat;
+    private Protein protein;
+
+    protected Meal(Carb carb, Fat fat, Protein protein) {
+        this.carb = carb;
+        this.fat = fat;
+        this.protein = protein;
+    }
+
+    public boolean isCarbAllowedByDiet(DietRestriction dietRestriction) {
+        return carb.isDietAllowed(dietRestriction);
+    }
+
+    public boolean isFatAllowedByDiet(DietRestriction dietRestriction) {
+        return fat.isDietAllowed(dietRestriction);
+    }
+
+    public boolean isProteinAllowedByDiet(DietRestriction dietRestriction) {
+        return protein.isDietAllowed(dietRestriction);
+    }
+
+    public void setCarb(Carb carb) {
+        this.carb = carb;
+    }
+
+    public void setFat(Fat fat) {
+        this.fat = fat;
+    }
+
+    public void setProtein(Protein protein) {
+        this.protein = protein;
+    }
+
+    @Override
+    protected Meal clone() {
+        try {
+            return (Meal) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError("Cannot clone Meal");
+        }
+    }
 }
--- src/main/java/org/CPPFoodDelivery/meal/MealFactory.java
+++ src/main/java/org/CPPFoodDelivery/meal/MealFactory.java
@@ -1,11 +1,36 @@
 package org.CPPFoodDelivery.meal;
 
+import org.CPPFoodDelivery.meal.macros.carb.Carb;
+import org.CPPFoodDelivery.meal.macros.carb.CarbFactory;
+import org.CPPFoodDelivery.meal.macros.fat.Fat;
+import org.CPPFoodDelivery.meal.macros.fat.FatFactory;
+import org.CPPFoodDelivery.meal.macros.protein.Protein;
+import org.CPPFoodDelivery.meal.macros.protein.ProteinFactory;
+
 public class MealFactory {
     public static Meal createMeal(String carbName, String fatName, String proteinName) {
-        return null; // TODO
+        Carb carb = CarbFactory.createCarbByName(carbName);
+        Fat fat = FatFactory.createFatByName(fatName);
+        Protein protein = ProteinFactory.createProteinByName(proteinName);
+
+        return new Meal(carb, fat, protein);
     }
 
     public static Meal createModifiedMeal(Meal baseMeal, DietRestriction dietRestriction) {
-        return null; // TODO
+        Meal newMeal = baseMeal.clone();
+
+        if (!newMeal.isCarbAllowedByDiet(dietRestriction)) {
+            newMeal.setCarb(CarbFactory.createCarbByDietRestriction(dietRestriction));
+        }
+
+        if (!newMeal.isFatAllowedByDiet(dietRestriction)) {
+            newMeal.setFat(FatFactory.createFatByDietRestriction(dietRestriction));
+        }
+
+        if (!newMeal.isProteinAllowedByDiet(dietRestriction)) {
+            newMeal.setProtein(ProteinFactory.createProteinByDietRestriction(dietRestriction));
+        }
+
+        return newMeal;
     }
 }
