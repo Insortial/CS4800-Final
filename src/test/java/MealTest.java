747146e1396e2ed979999dea90fc715f97a4fb14
Some-Regular-Person <31082683+Justin-J-K@users.noreply.github.com>
2024-05-17 21:07:47 -0700

    Meal test

--- src/test/java/MealTest.java
+++ src/test/java/MealTest.java
@@ -1,7 +1,25 @@
+import org.CPPFoodDelivery.meal.DietRestriction;
 import org.CPPFoodDelivery.meal.Meal;
 import org.CPPFoodDelivery.meal.MealFactory;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class MealTest {
     private Meal meal = MealFactory.createMeal("Bread", "Avocado", "Beef");
+
+    @Test
+    public void canCheckIsCarbAllowedByDiet() {
+        assertTrue(meal.isCarbAllowedByDiet(DietRestriction.NO_RESTRICTION));
+    }
+
+    @Test
+    public void canCheckIsFatAllowedByDiet() {
+        assertTrue(meal.isFatAllowedByDiet(DietRestriction.NO_RESTRICTION));
+    }
+
+    @Test
+    public void canCheckIsProteinAllowedByDiet() {
+        assertTrue(meal.isProteinAllowedByDiet(DietRestriction.NO_RESTRICTION));
+    }
 }
